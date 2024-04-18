f84cdcc1727d507bc4c5f7ac2f52cdf3ca64e40d
samirkarimov022 <samir.karimov0022@gmail.com>
2024-04-18 05:58:40 +0400

    add task

---
+++ ListUlviMWork/Moduls/IntArrayList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListUlviMWork.Moduls
+{
+    internal class IntArrayList
+    {
+        int[] _arr;
+        public int Capasity {  get; set; }
+        public int Count { get; set; }
+        public int this[int num]
+        {
+            get
+            {
+                return _arr[Count];
+            }
+            set
+            {
+                if (value <= Count)
+                {
+                _arr[Count] = value;
+                }
+                else
+                {
+                    Console.WriteLine("array bitib");
+                }
+            }
+        }
+        public IntArrayList()
+        {
+            Capasity = 5;
+            _arr =new int[Capasity];
+        } public IntArrayList(int capasity)
+        {
+            Capasity = capasity;
+            _arr =new int[Capasity];
+        }
+        public void Add(int value)
+        {
+            if(_arr.Length == Count)
+            {
+                Array.Resize(ref _arr,Count + Capasity);
+            }
+                _arr[Count] = value;
+                Count++;
+        }
+        public void GetInfo() 
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(_arr[i]);
+            }
+        }
+        public void IndexOf(int value)
+        {
+            bool result = false;
+            int index = 0;
+            for (int i = 0;i < Count;i++)
+            {
+                if (_arr[i] == value)
+                {
+                    index = i;
+                    result = true;
+                    break;
+                }
+                
+            }
+            if (result)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("arrayda tapilmadi");
+
+            } 
+
+
+        }
+        public void LastIndexOf(int value2)
+        {
+            
+            bool result = false;
+            int index = 0;
+            for (int i = Count;i > 0;i--)
+            {
+                if (_arr[i] == value2)
+                {
+                    index = i;
+                    result = true;
+                    break;
+                }
+                
+            }
+            if (result)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("arrayda tapilmadi");
+            }
+            
+
+        }
+        public void RemoveAt(int index)
+        {
+            _arr[index] = 0;
+        }
+
+
+        public override string ToString()
+        {
+            string result = null;
+            for( int i = 0; i < Count;i++ )
+            {
+                if (i > 0)
+                {
+                    result += ", ";
+                }
+                result += _arr[i].ToString();
+
+            }
+            return result;
+        }
+    }
+}
---
+++ ListUlviMWork/Program.cs
@@ -0,0 +1,43 @@
+﻿using ListUlviMWork.Moduls;
+
+namespace ListUlviMWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IntArrayList list = new IntArrayList();
+            
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(3);
+            list.Add(6);
+
+            list.GetInfo();
+            Console.WriteLine("");
+
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("");
+            list.IndexOf(3);
+            list.IndexOf(8);
+
+            Console.WriteLine("");
+
+            list.LastIndexOf(2);
+            list.LastIndexOf(8);
+            Console.WriteLine("");
+
+            list.RemoveAt(0);
+            list.RemoveAt(1);
+            list.RemoveAt(3);
+            list.GetInfo();
+
+
+
+
+        }
+    }
+}
